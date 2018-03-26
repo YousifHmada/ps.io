19e3286389dae333510de11e91e5eb711898754c
Yousif Hmada <1401724@eng.asu.edu.eg>
2018-03-26 20:02:46 +0200

    =adding main structure

--- src/App.js
+++ src/App.js
@@ -2,52 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function ProcessList(props) {
-  const results = props.items.map((value, index)=>
-      <a key={'item_' + index} className="col">{value}</a>
-  );
-  return (
-      results
-  )
-}
-
-
-function ProcessBoard(props) {
-  const results = props.items.map((value, index)=>
-    <div key={'list_' + index} className="connected row">
-        <ProcessList items={value}/>
-    </div>
-  );
-  return (
-    results
-  )
-}
-
-class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [['p1', 'p2', 'p3'], ['p2', 'p3'], ['p1', 'p2', 'p3', 'p4']]
-    }
-  }
-  render() {
-    return (
-      <section id="connected">
-        <ProcessBoard items={this.state.items}/>
-      </section>
-    )
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Board />
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
